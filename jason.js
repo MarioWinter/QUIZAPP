d54c3c2d335e0ea74b8ffcd7136188374595296a
Mario Müller <mariomuller@macbook-pro.speedport.ip>
2023-09-03 15:34:36 +0200

    add bootstrap card and json questions

---
+++ jason.js
@@ -0,0 +1,337 @@
+let htmlQuestions = [
+    {
+        "question": "Was steht für HTML?",
+        "answer_1": "Hyperlink and Text Markup Language",
+        "answer_2": "Hyper Text Markup Language",
+        "answer_3": "Hyper Transfer Markup Language",
+        "answer_4": "Highly Typed Multiple Language",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um Überschriften darzustellen?",
+        "answer_1": "<head>",
+        "answer_2": "<title>",
+        "answer_3": "<h1>",
+        "answer_4": "<p>",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist der Zweck des HTML-<div>-Elements?",
+        "answer_1": "Um eine abgegrenzte Liste von Elementen zu erstellen",
+        "answer_2": "Um Text fett zu machen",
+        "answer_3": "Um eine Absatzmarkierung zu erstellen",
+        "answer_4": "Um Bereiche für Layout und Gruppierung von Elementen zu erstellen",
+        "right_answer": 4
+    },
+    {
+        "question": "Was ist HTML5?",
+        "answer_1": "Die neueste Version von HTML",
+        "answer_2": "Ein Videocodierungsformat",
+        "answer_3": "Ein neuer Webbrowser",
+        "answer_4": "Eine Computersprache",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche Dateierweiterung wird normalerweise für HTML-Dokumente verwendet?",
+        "answer_1": ".exe",
+        "answer_2": ".html",
+        "answer_3": ".txt",
+        "answer_4": ".pdf",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um einen Link zu einer anderen Webseite zu erstellen?",
+        "answer_1": "<link>",
+        "answer_2": "<a>",
+        "answer_3": "<href>",
+        "answer_4": "<url>",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie wird ein Kommentar in HTML geschrieben?",
+        "answer_1": "// Dies ist ein Kommentar",
+        "answer_2": "/* Dies ist ein Kommentar */",
+        "answer_3": "<!-- Dies ist ein Kommentar -->",
+        "answer_4": "# Dies ist ein Kommentar",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist der Zweck des HTML-<img>-Elements?",
+        "answer_1": "Um Bilder in eine Webseite einzufügen",
+        "answer_2": "Um Audio in eine Webseite einzufügen",
+        "answer_3": "Um Videos in eine Webseite einzufügen",
+        "answer_4": "Um Text in eine Webseite einzufügen",
+        "right_answer": 1
+    },
+    {
+        "question": "Was bedeutet CSS in Bezug auf HTML?",
+        "answer_1": "Computer Style Sheet",
+        "answer_2": "Cascading Style Sheet",
+        "answer_3": "Creative Style Sheet",
+        "answer_4": "Colorful Style Sheet",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um eine geordnete Liste zu erstellen?",
+        "answer_1": "<ol>",
+        "answer_2": "<ul>",
+        "answer_3": "<li>",
+        "answer_4": "<dl>",
+        "right_answer": 1
+    }
+];
+
+
+let cssQuestions = [
+    {
+        "question": "Welche CSS-Eigenschaft wird verwendet, um die Schriftfarbe eines Textes festzulegen?",
+        "answer_1": "color",
+        "answer_2": "font-color",
+        "answer_3": "text-color",
+        "answer_4": "style-color",
+        "right_answer": 1
+    },
+    {
+        "question": "Was bedeutet die Abkürzung 'CSS'?",
+        "answer_1": "Computer Style Sheet",
+        "answer_2": "Cascading Style Sheet",
+        "answer_3": "Creative Style Sheet",
+        "answer_4": "Colorful Style Sheet",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche CSS-Eigenschaft wird verwendet, um den Abstand zwischen den Zeilen in einem Text festzulegen?",
+        "answer_1": "line-height",
+        "answer_2": "text-spacing",
+        "answer_3": "line-spacing",
+        "answer_4": "font-line",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie erstellen Sie in CSS einen Kommentar?",
+        "answer_1": "// Dies ist ein Kommentar",
+        "answer_2": "/* Dies ist ein Kommentar */",
+        "answer_3": "<!-- Dies ist ein Kommentar -->",
+        "answer_4": "# Dies ist ein Kommentar",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche CSS-Eigenschaft wird verwendet, um den Rand eines Elements zu definieren?",
+        "answer_1": "border-style",
+        "answer_2": "border-width",
+        "answer_3": "border-color",
+        "answer_4": "border",
+        "right_answer": 4
+    },
+    {
+        "question": "Was ist das CSS-Box-Modell?",
+        "answer_1": "Ein Modell für das Layout von Webseiten",
+        "answer_2": "Eine CSS-Eigenschaft",
+        "answer_3": "Eine JavaScript-Bibliothek",
+        "answer_4": "Ein CSS-Framework",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche CSS-Eigenschaft wird verwendet, um den Hintergrund eines Elements zu definieren?",
+        "answer_1": "background-image",
+        "answer_2": "background-color",
+        "answer_3": "background",
+        "answer_4": "element-background",
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet 'Cascading' in Cascading Style Sheets (CSS)?",
+        "answer_1": "Eingebettet",
+        "answer_2": "Verkettet",
+        "answer_3": "Verschachtelt",
+        "answer_4": "Kaskadierend",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche Einheit wird in CSS verwendet, um die Schriftgröße festzulegen?",
+        "answer_1": "px",
+        "answer_2": "em",
+        "answer_3": "pt",
+        "answer_4": "in",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche CSS-Eigenschaft wird verwendet, um Text in Großbuchstaben umzuwandeln?",
+        "answer_1": "text-transform",
+        "answer_2": "uppercase",
+        "answer_3": "text-case",
+        "answer_4": "transform-case",
+        "right_answer": 1
+    }
+];
+
+
+let javascriptQuestions = [
+    {
+        "question": "Was ist JavaScript?",
+        "answer_1": "Eine Kaffeemaschine",
+        "answer_2": "Eine Programmiersprache",
+        "answer_3": "Ein neuer Superheld",
+        "answer_4": "Ein Haustier",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist eine Variable in JavaScript?",
+        "answer_1": "Ein Vogel",
+        "answer_2": "Ein Container für Daten",
+        "answer_3": "Eine Mathematikaufgabe",
+        "answer_4": "Ein Lied",
+        "right_answer": 2
+    },
+    {
+        "question": "Was bedeutet 'DOM' in JavaScript?",
+        "answer_1": "Dancing Octopus Music",
+        "answer_2": "Document Object Model",
+        "answer_3": "Dark Orange Mango",
+        "answer_4": "Digital Online Marketing",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Schlüsselwort wird in JavaScript verwendet, um eine Funktion zu definieren?",
+        "answer_1": "cat",
+        "answer_2": "function",
+        "answer_3": "banana",
+        "answer_4": "sunshine",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist ein 'Array' in JavaScript?",
+        "answer_1": "Ein spezieller Drucker",
+        "answer_2": "Eine Sammlung von Elementen",
+        "answer_3": "Ein Berg in den Alpen",
+        "answer_4": "Ein Buch",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche Art von Sprache ist JavaScript?",
+        "answer_1": "Eine Geheimsprache der Illuminaten",
+        "answer_2": "Eine Programmiersprache für Webentwicklung",
+        "answer_3": "Eine Vogelsprache",
+        "answer_4": "Eine Kochsprache",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche Funktion wird verwendet, um eine Ausgabe in die Konsole zu schreiben?",
+        "answer_1": "console.write()",
+        "answer_2": "output()",
+        "answer_3": "print()",
+        "answer_4": "console.log()",
+        "right_answer": 4
+    },
+    {
+        "question": "Was macht die JavaScript-Bibliothek 'jQuery'?",
+        "answer_1": "Einen Kuchen backen",
+        "answer_2": "HTML-Elemente bearbeiten und animieren",
+        "answer_3": "Fußball spielen",
+        "answer_4": "Gedichte schreiben",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist eine 'Schleife' in JavaScript?",
+        "answer_1": "Eine Verbindung zwischen zwei Punkten",
+        "answer_2": "Eine wiederholte Ausführung von Code",
+        "answer_3": "Eine Achterbahnfahrt",
+        "answer_4": "Ein Kreis",
+        "right_answer": 2
+    },
+    {
+        "question": "Was bedeutet 'AJAX' in JavaScript?",
+        "answer_1": "Eine Marke von Haushaltsreinigern",
+        "answer_2": "Asynchronous JavaScript and XML",
+        "answer_3": "Eine exotische Frucht",
+        "answer_4": "Ein griechischer Gott",
+        "right_answer": 2
+    }
+];
+
+
+let pythonQuestions = [
+    {
+        "question": "Was ist Python?",
+        "answer_1": "Eine Schlange",
+        "answer_2": "Eine Insel",
+        "answer_3": "Eine Programmiersprache",
+        "answer_4": "Eine Frucht",
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet 'print' in Python?",
+        "answer_1": "Einen Tintenstrahldrucker",
+        "answer_2": "Etwas, das du isst",
+        "answer_3": "Eine Funktion zum Ausgeben von Text",
+        "answer_4": "Eine Art von Fußbekleidung",
+        "right_answer": 3
+    },
+    {
+        "question": "Welche Art von Sprache ist Python?",
+        "answer_1": "Eine Geheimsprache der Geeks",
+        "answer_2": "Eine Programmiersprache für Anfänger und Experten",
+        "answer_3": "Eine Vogelsprache",
+        "answer_4": "Eine Sportart",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist eine 'Schleife' in Python?",
+        "answer_1": "Ein Seil",
+        "answer_2": "Ein Zauberspruch",
+        "answer_3": "Eine wiederholte Ausführung von Code",
+        "answer_4": "Eine Art von Musik",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist 'Pythonic'?",
+        "answer_1": "Eine Schlafposition",
+        "answer_2": "Ein Schlangenbiss",
+        "answer_3": "Ein Begriff, der sauberen und leserlichen Python-Code beschreibt",
+        "answer_4": "Ein Gericht aus Python-Schlangen",
+        "right_answer": 3
+    },
+    {
+        "question": "Welches Schlüsselwort wird in Python verwendet, um eine Funktion zu definieren?",
+        "answer_1": "dog",
+        "answer_2": "function",
+        "answer_3": "magic",
+        "answer_4": "sunshine",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist 'PEP 8' in Python?",
+        "answer_1": "Eine Art von Python-Schlange",
+        "answer_2": "Ein Leitfaden für sauberen und leserlichen Python-Code",
+        "answer_3": "Ein Pythonspiel",
+        "answer_4": "Ein Code-Editor",
+        "right_answer": 2
+    },
+    {
+        "question": "Was macht die Python-Bibliothek 'numpy'?",
+        "answer_1": "Einen Kuchen backen",
+        "answer_2": "Mathematische Operationen mit Arrays und Matrizen",
+        "answer_3": "Tiergeräusche",
+        "answer_4": "Geschäftsberichte",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist 'Python 2'?",
+        "answer_1": "Die zweite Version der Schlange",
+        "answer_2": "Eine veraltete Version von Python",
+        "answer_3": "Eine Fortsetzung des Films 'Monty Python and the Holy Grail'",
+        "answer_4": "Eine neue Schlange im Zoo",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist 'Python 3'?",
+        "answer_1": "Die dritte Version der Schlange",
+        "answer_2": "Eine Sammlung von Pythonsprichwörtern",
+        "answer_3": "Eine Fernsehshow",
+        "answer_4": "Eine Python-Skulptur",
+        "right_answer": 1
+    }
+];
+
+
+
